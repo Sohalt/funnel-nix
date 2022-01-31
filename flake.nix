824e4b8bc7faff9d8e3142deeda3e9c0f3974dc6
sohalt <sohalt@sohalt.net>
2022-01-31 14:04:36 +0100

    funnel: init at v1.4.71

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Transit-over-WebSocket Message Hub";
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      supportedSystems = ["x86_64-linux" "x86_64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (system: {
+        funnel = let
+          pkgs = import nixpkgs { inherit system; };
+          jdk = pkgs.graalvm17-ce;
+          clojure = pkgs.clojure.override { inherit jdk; };
+          cljdeps = import ./deps.nix { inherit (pkgs) fetchMavenArtifact fetchgit lib; };
+        in
+          pkgs.stdenv.mkDerivation rec {
+            pname = "funnel";
+            version = "1.4.71";
+
+            src = pkgs.fetchFromGitHub {
+              owner = "lambdaisland";
+              repo = "funnel";
+              rev = "v${version}";
+              sha256 = "sha256-h+3SlXu1W5AyMX4iGehm1W4JXNjiFnvixkJiKIrT4e8=";
+            };
+            buildInputs =  [ clojure jdk ];
+            HOME = "$TMPDIR";
+
+            patches = [
+              (pkgs.substituteAll {
+                src = ./fix-classpath.patch;
+                classpath = cljdeps.makeClasspaths { extraClasspaths = [ "src" "lib"]; };
+              })
+            ];
+
+            installPhase = ''
+              install -D funnel $out/bin/funnel
+            '';
+          };
+      });
+      defaultPackage = forAllSystems (system: self.packages."${system}".funnel);
+    };
+}
